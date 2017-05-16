ddfb3a5c5e47a81b16cb868fdbd9516f71c39f9e
ambarwootric <amb.ar4eve@gmail.com>
2017-05-16 14:22:52 +0545

    configure segment and filters

--- app/src/main/java/com/acxelense/wootriccsat/MainActivity.java
+++ app/src/main/java/com/acxelense/wootriccsat/MainActivity.java
@@ -3,13 +3,18 @@ package com.acxelense.wootriccsat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.wootric.androidsdk.Wootric;
 
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText country;
+    private EditText age;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void startSurvey(View view) {
 
-        // Integrating for registered user with external id
+        // Integrating segment and filters
         // Using CSAT account Client ID, Client Secret and account_token
         Wootric wootric = Wootric.init(this, "582cde53eaf427b977a11e2f7086243db79d0d29aeb5c03d10fb544e4b911c9d",
                 "857c872f31b14f5f52a34b81a70cf0c25506e1aaf9fb8fd929d981dd3fdd3014",
@@ -36,7 +41,15 @@ public class MainActivity extends AppCompatActivity {
         // User external id
         wootric.setEndUserExternalId("543");
 
+        // Segment and Filters using customer properties
+        country = (EditText)findViewById(R.id.country);
+        age = (EditText)findViewById(R.id.age);
 
+        HashMap<String, String> endUserDetails = new HashMap<String, String>();
+        endUserDetails.put("country", country.getText().toString());
+        endUserDetails.put("age_amount", age.getText().toString());
+
+        wootric.setProperties(endUserDetails);
 
         // Use only for testing
         wootric.setSurveyImmediately(true);
